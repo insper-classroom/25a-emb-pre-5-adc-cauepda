892f9c96343a9f953cde1ad9b83b2912b85bc080
cauepda <cauepda@al.insper.edu.br>
2025-03-31 09:18:10 -0300

    Add second LED task and update task creation in main ex2

--- exe2/main.c
+++ exe2/main.c
@@ -8,8 +8,9 @@
 #include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
+const int PWM_1_PIN = 6;
 
-void led_task(void *p) {
+void led0_task(void *p) {
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
     pwm_set_clkdiv(slice_num, 125);
@@ -20,12 +21,24 @@ void led_task(void *p) {
     while (true) {
     }
 }
+void led1_task(void *p) {
+    gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(PWM_1_PIN);
+    pwm_set_clkdiv(slice_num, 125);
+    pwm_set_wrap(slice_num, 100);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 20);
+    pwm_set_enabled(slice_num, true);
+
+    while (true) {
+    }
+}
 
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
-    xTaskCreate(led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led0_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led1_task, "LED_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
