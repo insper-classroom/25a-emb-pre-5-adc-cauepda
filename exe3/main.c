bb2e3d07ed0a772b311bf5eaf1125ae0b69960f9
cauepda <cauepda@al.insper.edu.br>
2025-04-09 15:20:11 -0300

    update final

--- exe3/main.c
+++ exe3/main.c
@@ -4,11 +4,11 @@
 
 #include "pico/stdlib.h"
 #include <stdio.h>
-
 #include "data.h"
+
 QueueHandle_t xQueueData;
 
-// não mexer! Alimenta a fila com os dados do sinal
+// Não mexer! Alimenta a fila com os dados do sinal
 void data_task(void *p) {
     vTaskDelay(pdMS_TO_TICKS(400));
 
@@ -28,32 +28,31 @@ void sendToUART(int value) {
 
 void process_task(void *p) {
     int data = 0;
-    int len_mean = 5;
+    const int len_mean = 5;
     double fila_dividendo[len_mean];
     int index1 = 0;
     int sum = 0;
     int index2 = 0;
 
-
     while (true) {
-        if (xQueueReceive(xQueueData, &data, 100)) {
-            // implementar filtro aqui!
-                if (index1 < len_mean) {
-                    fila_dividendo[index1] = data;
-                    sum += data;
-                    index1++;
-
-                }
-                else {
-                    sendToUART(sum / len_mean);
-                    sum -= fila_dividendo[index2];
-                    sum += data;
-                    fila_dividendo[index2] = data;
-                    index2++;
-                    if (index2 >= len_mean)
-                        index2 = 0;
-                }
-            // deixar esse delay!
+        if (xQueueReceive(xQueueData, &data, pdMS_TO_TICKS(100))) {
+            // Implementar filtro: inicialmente acumula os dados
+            if (index1 < len_mean) {
+                fila_dividendo[index1] = data;
+                sum += data;
+                index1++;
+            }
+            else {
+                sendToUART(sum / len_mean);
+                // Atualiza a média com o novo valor, descartando o valor mais antigo
+                sum -= fila_dividendo[index2];
+                sum += data;
+                fila_dividendo[index2] = data;
+                index2++;
+                if (index2 >= len_mean)
+                    index2 = 0;
+            }
+            // Deixar esse delay!
             vTaskDelay(pdMS_TO_TICKS(50));
         }
     }
@@ -61,11 +60,12 @@ void process_task(void *p) {
 
 int main() {
     stdio_init_all();
+    printf("Start RTOS \n");
 
     xQueueData = xQueueCreate(64, sizeof(int));
 
-    xTaskCreate(data_task, "Data task ", 4096, NULL, 1, NULL);
-    xTaskCreate(process_task, "Process task", 4096, NULL, 1, NULL);
+    xTaskCreate(data_task, "Data task", 256, NULL, 1, NULL);
+    xTaskCreate(process_task, "Process task", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
